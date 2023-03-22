10f57e5b397ed685888ac91850216f97710c4afd
Simonebaca <simobaco99@gmail.com>
2023-03-22 16:59:17 +0100

    try-milestone2

--- main.js
+++ main.js
@@ -20,40 +20,53 @@ Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
 // Creare l'array di oggetti con le informazioni fornite.
 
 const team = [
-    {name : 'Wayne Barnett',
-    role : 'Founder & CEO',
-    image : 'wayne-barnett-founde-ceo.jpg'
-    }, 
-    {name : 'Angela Caroll',
-    role : 'Chief Editor',
-    image : 'angela-caroll-chief-editor.jpg',
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founde-ceo.jpg'
     },
-    {name : 'Walter Gordon',
-    role : 'Office Manager',
-    image : 'walter-gordon-office-manager.jpg'
-    }, 
-    {name : 'Angela Lopez',
-    role : 'Social Media Manager',
-    image : 'angela-lopez-social-media-manager.jpg',
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
     },
-    {name : 'Scott Estrada',
-    role : 'Developer',
-    image : 'scott-estrada-developer.jpg'
-    }, 
-    {name : 'Barbara Ramos',
-    role : 'Graphic Designer',
-    image : 'barbara-ramos-graphic-designer.jpg',
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
     }
 
 ];
 
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+let key;
 
-for (let key in team ){
-     console.log(team[key])
-     let container = document.getElementById('container').innerHTML =   team[key];
- }
+for (key in team) {
+    console.log(team[key])
 
+}
 //  MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+let cont = document.getElementById('container').innerHTML = team[key]
+
+
+// for(let i = 0 ; i < team.length ; i++){
+         // container.innerHTML = team{key}
+//     document.write(team[i])
+//}
