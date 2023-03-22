976f5f5454acf73a577ad8be73f63892ea654d23
Simonebaca <simobaco99@gmail.com>
2023-03-22 16:19:39 +0100

    milestone0&1

--- main.js
+++ main.js
@@ -16,3 +16,44 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
 
+// MILESTONE 0:
+// Creare l'array di oggetti con le informazioni fornite.
+
+const team = [
+    {name : 'Wayne Barnett',
+    role : 'Founder & CEO',
+    image : 'wayne-barnett-founde-ceo.jpg'
+    }, 
+    {name : 'Angela Caroll',
+    role : 'Chief Editor',
+    image : 'angela-caroll-chief-editor.jpg',
+    },
+    {name : 'Walter Gordon',
+    role : 'Office Manager',
+    image : 'walter-gordon-office-manager.jpg'
+    }, 
+    {name : 'Angela Lopez',
+    role : 'Social Media Manager',
+    image : 'angela-lopez-social-media-manager.jpg',
+    },
+    {name : 'Scott Estrada',
+    role : 'Developer',
+    image : 'scott-estrada-developer.jpg'
+    }, 
+    {name : 'Barbara Ramos',
+    role : 'Graphic Designer',
+    image : 'barbara-ramos-graphic-designer.jpg',
+    }
+
+];
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+for (let key in team ){
+     console.log(team[key])
+     let container = document.getElementById('container').innerHTML =   team[key];
+ }
+
+//  MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
